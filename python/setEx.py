0b7621521ca8d44765e033fb56537d9662f09a4d
RRaynes <cameronr252@gmail.com>
2024-06-19 08:30:02 -0500

    examples of how to use lists in python

---
+++ python/setEx.py
@@ -0,0 +1,8 @@
+correctAnswers = []
+
+answer = int(input("What is 1+1"))
+if (answer == 2): #
+    correctAnswers.append("What is 1+1")
+
+print("here are the questions you got correct")
+print(correctAnswers[0])
