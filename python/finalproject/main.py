483cd7c7a687b03b07adb667265b8aab6cb9a818
RRaynes <cameronr252@gmail.com>
2024-06-19 11:22:17 -0500

    sprite creation

--- python/finalproject/main.py
+++ python/finalproject/main.py
@@ -7,10 +7,19 @@ height = 480
 screen = pygame.display.set_mode((width, height))
 #making width and height of screen
 
-bg_color = (45,76,180) #making background color
+bg_color = (255,255,255) #making background color
 
-sprite = pygame.image.load('images/slush.png')
 spriteX = 10 #this is the initial x position of the sprite
+spriteY = 40 #initial y position
+spriteWidth = 640
+spriteHeight = 640
+sprite = pygame.image.load('images/slush.png')
+sprite = pygame.transform.scale(sprite, (spriteWidth, spriteHeight))
+
+spriteColor = (90, 231, 125) #Green
+
+def draw_sprites():
+    screen.blit(sprite,(spriteX,spriteY))
 
 #MAIN GAME LOOP
 while True:
@@ -21,4 +30,6 @@ while True:
             #this if condition checks if the user is trying to quit the program and if they are
             #we use sys.exit() to close the window
     screen.fill(bg_color)
+    draw_sprites()
+
     pygame.display.flip() #updates screen
