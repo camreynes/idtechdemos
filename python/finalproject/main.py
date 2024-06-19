c2a305765acb86ebd668defcdf7409d45b5fcc57
RRaynes <cameronr252@gmail.com>
2024-06-19 15:26:24 -0500

    enemey init

--- python/finalproject/main.py
+++ python/finalproject/main.py
@@ -1,9 +1,10 @@
 import pygame, sys
+from pygame import key
 
 pygame.init() #initialize pygame
 
-width = 640
-height = 480
+width = 800
+height = 800
 screen = pygame.display.set_mode((width, height))
 #making width and height of screen
 
@@ -14,17 +15,38 @@ spriteY = 40 #initial y position
 spriteWidth = 640
 spriteHeight = 640
 sprite = pygame.image.load('images/slush.png')
-sprite = pygame.transform.scale(sprite, (spriteWidth, spriteHeight))
+enemy_image = pygame.image.load('images/enemy.png')
 
+sprite = pygame.transform.scale(sprite, (spriteWidth, spriteHeight))
+enemy_image = pygame.transform.scale(enemy_image, (500,500))
 spriteColor = (90, 231, 125) #Green
 
+spriteObject = pygame.Rect(spriteX, spriteY, spriteWidth, spriteHeight)
+
+speed = 10
+
+clock = pygame.time.Clock()
+fps = 24
+
 def draw_sprites():
-    screen.blit(sprite,(spriteX,spriteY))
+    screen.blit(sprite,spriteObject)
 
 #MAIN GAME LOOP
 while True:
+    clock.tick(fps)
+
     #this for loop gets every input from the user and every event in the game
     for event in pygame.event.get():
+        key = pygame.key.get_pressed()
+        if key[pygame.K_w]: #if the key pressed is w
+            spriteObject.move_ip(0,-speed)
+        if key[pygame.K_s]:
+            spriteObject.move_ip(0,speed)
+        if key[pygame.K_a]:
+            spriteObject.move_ip(-speed,0)
+        if key[pygame.K_d]:
+            spriteObject.move_ip(speed,0)
+
         if event.type == pygame.QUIT:
             sys.exit()
             #this if condition checks if the user is trying to quit the program and if they are
