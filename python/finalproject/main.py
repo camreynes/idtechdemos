3f15d63a2f9a84cdac7725771dbfc45015668a39
RRaynes <cameronr252@gmail.com>
2024-06-19 09:21:29 -0500

    template for pygame

---
+++ python/finalproject/main.py
@@ -0,0 +1,21 @@
+import pygame, sys
+
+pygame.init() #initialize pygame
+
+width = 640
+height = 480
+screen = pygame.display.set_mode((width, height))
+#making width and height of screen
+
+bg_color = (45,76,180) #making background color
+
+#MAIN GAME LOOP
+while True:
+    #this for loop gets every input from the user and every event in the game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+            #this if condition checks if the user is trying to quit the program and if they are
+            #we use sys.exit() to close the window
+    screen.fill(bg_color)
+    pygame.display.flip() #updates screen
